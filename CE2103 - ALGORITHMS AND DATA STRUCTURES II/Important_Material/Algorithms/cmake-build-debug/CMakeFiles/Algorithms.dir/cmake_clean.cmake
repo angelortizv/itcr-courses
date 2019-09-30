280c723bfa320eec6cc0a8167955cea0deb80283
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-09-30 10:40:36 -0600

    [ADD] C Resources were added
    Book: The C Programming Language

---
+++ CE2103 - ALGORITHMS AND DATA STRUCTURES II/Important_Material/Algorithms/cmake-build-debug/CMakeFiles/Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algorithms.dir/main.cpp.o"
+  "Algorithms.pdb"
+  "Algorithms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
